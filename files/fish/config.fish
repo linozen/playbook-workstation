82e0a856acdebb7883c8ca1e29474d8cf7f4f496
linozen <linus@sehn.tech>
2022-10-10 14:39:53 +0200

    Major refactor
    Former-commit-id: 05c958e003c895decbd0bb9017a26739883d5dd4 [formerly 99364df272c9546ebac0aca3364da389ab69b2ae]
    Former-commit-id: 5c4c251e9c5446c5533eaed50994a3ee5e4b316a

--- files/fish/config.fish
+++ files/fish/config.fish
@@ -5,11 +5,6 @@ set pure_show_system_time true
 fish_add_path $HOME/.emacs.d/bin
 fish_add_path $HOME/.local/bin
 
-# environment variables
-set -x DOCKER_HOST "unix:///run/user/1000/docker.sock"
-set -x DOCKER_BUILDKIT 1
-set -x COMPOSE_DOCKER_CLI_BUILD 1
-
 if status is-interactive
     fish_vi_key_bindings
     # Emulates vim's cursor shape behavior
@@ -28,33 +23,6 @@ if status is-interactive
     asdf direnv hook fish | source
     cat ~/.cache/wal/sequences
 end
-
-# dracula theme
-set -U fish_color_normal normal
-set -U fish_color_command 50fa7b
-set -U fish_color_quote f1fa8c
-set -U fish_color_redirection 50fa7b
-set -U fish_color_end 50fa7b
-set -U fish_color_error ff5555
-set -U fish_color_param 8be9fd
-set -U fish_color_comment 5b6268
-set -U fish_color_match --background=brblue
-set -U fish_color_selection white --bold --background=brblack
-set -U fish_color_search_match bryellow --background=brblack
-set -U fish_color_history_current --bold
-set -U fish_color_operator bd93f9
-set -U fish_color_escape bd93f9
-set -U fish_color_cwd green
-set -U fish_color_cwd_root red
-set -U fish_color_valid_path --underline
-set -U fish_color_autosuggestion 555 brblack
-set -U fish_color_user brgreen
-set -U fish_color_host normal
-set -U fish_color_cancel -r
-set -U fish_pager_color_completion normal
-set -U fish_pager_color_description f1fa8c
-set -U fish_pager_color_prefix normal --bold --underline
-set -U fish_pager_color_progress brwhite --background=cyan
 
 # TODO aliases
 alias ..='cd ..'
@@ -89,9 +57,6 @@ alias gurl='curl --compressed'
 
 alias shutdown='sudo shutdown'
 alias reboot='sudo reboot'
-
-alias y='xclip -selection clipboard -in'
-alias p='xclip -selection clipboard -out'
 
 alias jc='journalctl -xe'
 alias sc=systemctl
